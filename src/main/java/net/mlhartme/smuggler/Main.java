dcf6551b2be54f5b9b305f025b468fe1633ced0d
Michael Hartmeier <michael@schmizzolin.de>
2016-11-17 20:37:17 +0100

    get album list

--- src/main/java/net/mlhartme/smuggler/Main.java
+++ src/main/java/net/mlhartme/smuggler/Main.java
@@ -1,8 +1,11 @@
 package net.mlhartme.smuggler;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.core.util.MultivaluedMapImpl;
 import com.sun.jersey.oauth.client.OAuthClientFilter;
 import com.sun.jersey.oauth.signature.OAuthParameters;
 import com.sun.jersey.oauth.signature.OAuthSecrets;
@@ -10,6 +13,8 @@ import com.sun.jersey.oauth.signature.OAuthSecrets;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class Main {
@@ -26,10 +31,12 @@ public class Main {
 		}
 	}
 
-	private static String CONSUMER_KEY = get("consumer.key");
-	private static String CONSUMER_SECRET = get("consumer.secret");
-	private static String OAUTH_TOKEN_ID = get("token.id");
-	private static String OAUTH_TOKEN_SECRET = get("token.secret");
+	private static final String CONSUMER_KEY = get("consumer.key");
+	private static final String CONSUMER_SECRET = get("consumer.secret");
+	private static final String OAUTH_TOKEN_ID = get("token.id");
+	private static final String OAUTH_TOKEN_SECRET = get("token.secret");
+
+	private static final String ALBUM = get("album");
 
 	private static String get(String key) {
 		String value;
@@ -42,12 +49,31 @@ public class Main {
 	}
 
 	public static void main(String[] args) throws IOException {
+		System.out.println(album(ALBUM).toString());
+
+	}
+
+	public static List<String> album(String album) throws IOException {
+		JsonObject obj;
+		JsonArray array;
+		List<String> result;
+
+		obj = request("api/v2/album/" + album + "!images").getAsJsonObject();
+		array = obj.get("Response").getAsJsonObject().get("AlbumImage").getAsJsonArray();
+		result = new ArrayList<>();
+		for (JsonElement e : array) {
+			result.add(e.getAsJsonObject().get("FileName").getAsString());
+		}
+		return result;
+	}
+
+	public static JsonElement request(String path) throws IOException {
 		OAuthSecrets secrets;
 		OAuthParameters params;
 
 		System.out.println("test");
 
-		WebResource resource = CLIENT.resource("https://api.smugmug.com/api/v2/user/mlhartme!albums");
+		WebResource resource = CLIENT.resource("https://api.smugmug.com/" + path);
 		secrets = new OAuthSecrets().consumerSecret(CONSUMER_SECRET);
 		secrets.setTokenSecret(OAUTH_TOKEN_SECRET);
 		params = new OAuthParameters().consumerKey(CONSUMER_KEY).signatureMethod("HMAC-SHA1").version("1.0");
@@ -55,6 +81,6 @@ public class Main {
 		resource.addFilter(new OAuthClientFilter(CLIENT.getProviders(), params, secrets));
 
 		WebResource.Builder builder = resource.accept("application/json");
-		System.out.println(builder.get(String.class));
+		return new JsonParser().parse(builder.get(String.class));
 	}
 }
