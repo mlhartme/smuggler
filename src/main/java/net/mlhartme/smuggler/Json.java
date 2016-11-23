b09544e0c3ea442fdcb0fd38bf0ebf2626209443
Michael Hartmeier <mlhartme@github.com>
2016-11-23 20:12:11 +0100

    license

--- src/main/java/net/mlhartme/smuggler/Json.java
+++ src/main/java/net/mlhartme/smuggler/Json.java
@@ -15,29 +15,5 @@
  */
 package net.mlhartme.smuggler;
 
-import java.util.List;
-
-public class Image {
-    public static Image lookupFileName(List<Image> images, String fileName) {
-        for (Image image : images) {
-            if (image.fileName.equals(fileName)) {
-                return image;
-            }
-        }
-        return null;
-    }
-
-    //--
-
-    public final String key;
-    public final String fileName;
-
-    public Image(String key, String fileName) {
-        this.key = key;
-        this.fileName = fileName;
-    }
-
-    public void delete(Smugmug smugmug) {
-        smugmug.resource("https://api.smugmug.com/api/v2/image/" + key).delete();
-    }
+public class Json {
 }
